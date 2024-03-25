63a97442386c69e96ad4d1bb3c72f4d92562ec2a
giordano.daloisio <giordano.daloisio@gmail.com>
2024-03-25 16:07:23 +0000

    add tinyllama explanations

--- src/llama.py
+++ src/llama.py
@@ -1,44 +1,12 @@
-import ollama
-import pandas as pd
-import os
-from utils import parseAlgorithmCharacteristics
-
-ollama.pull("llama2")
-
-
-def runPrompt(complexPrompt):
-    for root, dirs, files in os.walk("../Dataset/Code"):
-        for filename in files:
-            print("Processing file: " + filename)
-            inFile = open(os.path.join(root, filename), "r", encoding="utf-8")
-            code = inFile.read()
-            prompt = "Can you give a high-level explanation of this code?" + code
-            if complexPrompt:
-                algorithmName, cubits, desc = parseAlgorithmCharacteristics(filename)
-                prompt += "The name of the algorithm is: " + algorithmName + "\n"
-                prompt += "The code includes " + cubits + " cubits" + "\n"
-            messages = [{"role": "user", "content": prompt}]
-
-            response = ollama.chat(
-                model="llama2",
-                messages=messages,
-            )["message"]
-
-            content = response["content"]
-            os.makedirs(os.getcwd() + "/../Output/llama2/", exist_ok=True)
-            out = open(
-                os.getcwd()
-                + "/../Output/llama2/"
-                + filename
-                + "_"
-                + str(complexPrompt),
-                "w",
-                encoding="utf-8",
-            )
-            out.write(content)
-            print(content)
-
+from utils import runPrompt
+from argparse import ArgumentParser
+import argparse
 
 if __name__ == "__main__":
-    runPrompt(True)
-    runPrompt(False)
+    parser = ArgumentParser()
+    parser.add_argument("--complex", action="store_true")
+    args = parser.parse_args()
+
+    base_model = "llama2"
+    runPrompt(args.complex, base_model)
+    # runPrompt(False)
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import ollama
+import os
 
 
 def parseAlgorithmCharacteristics(algo):
@@ -9,3 +11,39 @@ def parseAlgorithmCharacteristics(algo):
     cubits = filtered_df["Number of Qubits"].values[0]
     desc = filtered_df["One line description"].values[0]
     return str(algorithmName), str(cubits), str(desc)
+
+
+def runPrompt(complexPrompt, base_model):
+    ollama.pull(base_model)
+    for filename in os.listdir("../Dataset/Code"):
+        if filename.endswith(".qasm"):
+            print("Processing file: " + filename)
+            inFile = open(
+                os.path.join("../Dataset/Code", filename), "r", encoding="utf-8"
+            )
+            code = inFile.read()
+            prompt = "Can you give a high-level explanation of this code?" + code
+            if complexPrompt:
+                algorithmName, cubits, desc = parseAlgorithmCharacteristics(filename)
+                prompt += "The name of the algorithm is: " + algorithmName + "\n"
+                prompt += "The code includes " + cubits + " cubits" + "\n"
+            messages = [{"role": "user", "content": prompt}]
+
+            response = ollama.chat(
+                model=base_model,
+                messages=messages,
+            )["message"]
+
+            content = response["content"]
+            os.makedirs(os.getcwd() + f"/../Output/{base_model}", exist_ok=True)
+            out = open(
+                os.getcwd()
+                + f"/../Output/{base_model}/"
+                + filename
+                + "_"
+                + str(complexPrompt),
+                "w",
+                encoding="utf-8",
+            )
+            out.write(content)
+            print(content)
