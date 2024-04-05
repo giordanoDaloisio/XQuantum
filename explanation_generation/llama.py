131925e11d41d7a80b0ee230c9114f5df8f4bc11
carolhanna01 <carol.z.hanna@gmail.com>
2024-04-05 18:00:05 +0100

    adding support for iterative prompt optimization using 2nd llm

--- explanation_generation/llama.py
+++ explanation_generation/llama.py
@@ -5,6 +5,7 @@ import argparse
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--complex", action="store_true")
+    parser.add_argument("--iterative", action="store_true")
     args = parser.parse_args()
 
     base_model = "llama2"
@@ -12,5 +13,6 @@ if __name__ == "__main__":
         print("Use complex prompt")
     else:
         print("Use simple prompt")
-    runPrompt(args.complex, base_model)
-    # runPrompt(False)
+
+    # runPrompt(args.complex, args.iterative, 3, base_model)
+    runPrompt(args.complex, args.iterative, 10, base_model)
